8cdbc6b810a2174df1f3b3bfd992adbd29dbda08
Zach Blass <zblass@brandshare.us>
2015-10-06 15:10:45 -0400

    Heroku

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,18 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
 
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+ has_attached_file :image, 
+            :style => { :medium => "300x300>", :thumb => "100x100>" },
+            :storage => :s3,
+            :bucket  => ENV['MY_BUCKET_NAME'],
+            :s3_credentials => {
+                    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                },
+                :s3_permissions => 'private',
+                :url => ":s3_domain_url"
+    validates :image, presence: true
+    validates :description, presence: true
+
+
 end
