ba01bc4dca60e723b0b9f9910bd7864e1f655b49
Zach Blass <zblass@brandshare.us>
2015-12-01 14:10:10 -0500

    pay to tweet

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -51,7 +51,7 @@ class PinsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
+   def pin_params
       params.require(:pin).permit(:description, :image)
     end
 end
